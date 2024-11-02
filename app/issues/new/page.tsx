67c015cc31f458fd49df904a2eb60dde1fbbc7ea
Subash Rodrigo <subash.rodrigo@gmail.com>
2024-11-03 02:03:30 +0530

    Implemented client side validation for issue form input fields.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,24 +1,31 @@
 "use client";
 
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { Controller, useForm } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchema";
+import { z } from "zod";
 
-interface NewIssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
-  const { register, control, handleSubmit } = useForm<NewIssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const router = useRouter();
   const [error, setError] = useState<string | null>(null);
 
-  const onCreateIssue = async (data: NewIssueForm) => {
+  const onCreateIssue = async (data: IssueForm) => {
     try {
       await axios.post("/api/issues", data);
       router.push("/issues");
@@ -38,16 +45,30 @@ const NewIssuePage = () => {
         <TextField.Root
           variant="surface"
           placeholder="Title"
+          color={errors.title ? "red" : undefined}
           {...register("title")}
         />
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title.message}
+          </Text>
+        )}
         <Controller
           name="description"
           control={control}
           render={({ field }) => (
-            <SimpleMDE placeholder="Description" {...field} />
+            <SimpleMDE
+              className={errors.description ? "validation-failed" : ""}
+              placeholder="Description"
+              {...field}
+            />
           )}
         />
-
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description.message}
+          </Text>
+        )}
         <Button>Submit New Issue</Button>
       </form>
     </div>
