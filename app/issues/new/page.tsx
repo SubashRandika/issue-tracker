ac23b7dfc78f24ba36ef9b2a7a70c53fe07d2fb5
Subash Rodrigo <subash.rodrigo@gmail.com>
2024-11-02 16:49:28 +0530

    Implemented functionality create new issue with react-hook-form and axios to integrate with back-end endpoint.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,15 +2,42 @@
 
 import { Button, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
+import { Controller, useForm } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface NewIssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const { register, control, handleSubmit } = useForm<NewIssueForm>();
+  const router = useRouter();
+
+  const onCreateIssue = async (data: NewIssueForm) => {
+    await axios.post("/api/issues", data);
+    router.push("/issues");
+  };
+
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root variant="surface" placeholder="Title" />
-      <SimpleMDE placeholder="Description" />
+    <form className="max-w-xl space-y-3" onSubmit={handleSubmit(onCreateIssue)}>
+      <TextField.Root
+        variant="surface"
+        placeholder="Title"
+        {...register("title")}
+      />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
+
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
