2c9d6354d638eff6b9c40f0e9be68f1991e8700a
Subash Rodrigo <subash.rodrigo@gmail.com>
2024-11-18 01:58:52 +0530

    Created loading screens for issue details page and new issue creation page.

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,5 +1,20 @@
+import { Box, Card, Flex } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 const LoadingIssueDetails = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Flex gap="3" align="center" mt="2">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card className="prose flex flex-col" mt="4" size="3">
+        <Skeleton count={3} />
+      </Card>
+    </Box>
+  );
 };
 
 export default LoadingIssueDetails;
