b4f06d921d3d92158abcd6f0b4a1ca6bba2a7a37
Subash Rodrigo <subash.rodrigo@gmail.com>
2024-12-30 17:06:43 +0530

    Add toast notifications for deletion errors in DeleteIssueButton.

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,48 +1,90 @@
 "use client";
 
-import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import * as Toast from "@radix-ui/react-toast";
+import { AlertDialog, Button, Flex, Text } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { FaTrashAlt } from "react-icons/fa";
+import styles from "./Toast.module.css";
+import { GrClose } from "react-icons/gr";
+import { MdError } from "react-icons/md";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
+  const [error, setError] = useState(false);
 
   const handleDeleteIssue = async () => {
-    await axios.delete(`/api/issues/${issueId}`);
-    router.push("/issues");
-    router.refresh();
+    try {
+      await axios.delete(`/api/issues/${issueId}`);
+      router.push("/issues");
+      router.refresh();
+    } catch (error) {
+      setError(true);
+    }
   };
 
   return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color="red">
-          <FaTrashAlt size={16} />
-          Delete Issue
-        </Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content maxWidth="450px">
-        <AlertDialog.Title>Delete Issue</AlertDialog.Title>
-        <AlertDialog.Description size="2">
-          Are you sure you want to delete this issue? Once deleted you cannot
-          undone.
-        </AlertDialog.Description>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">
+            <FaTrashAlt size={16} />
+            Delete Issue
+          </Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content maxWidth="450px">
+          <AlertDialog.Title>Delete Issue</AlertDialog.Title>
+          <AlertDialog.Description size="2">
+            Are you sure you want to delete this issue? Once deleted you cannot
+            undone.
+          </AlertDialog.Description>
 
-        <Flex gap="3" mt="4" justify="end">
-          <AlertDialog.Cancel>
-            <Button variant="soft" color="gray">
-              Cancel
+          <Flex gap="3" mt="4" justify="end">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button variant="solid" color="red" onClick={handleDeleteIssue}>
+                Delete
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <Toast.Provider swipeDirection="down">
+        <Toast.Root
+          className={styles.Root}
+          open={error}
+          onOpenChange={setError}
+        >
+          <Toast.Title className={styles.Title}>
+            <Flex gap="2">
+              <MdError className={styles.Icon} size={24} />
+              <Text size="2">Deletion Failed</Text>
+            </Flex>
+          </Toast.Title>
+          <Toast.Description asChild>
+            <Text className={styles.Description} size="1">
+              This issue cannot be deleted. Please try again later.
+            </Text>
+          </Toast.Description>
+          <Toast.Action className={styles.Action} asChild altText="Close Toast">
+            <Button
+              size="1"
+              variant="soft"
+              color="gray"
+              onClick={() => setError(false)}
+            >
+              <GrClose />
             </Button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button variant="solid" color="red" onClick={handleDeleteIssue}>
-              Delete
-            </Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+          </Toast.Action>
+        </Toast.Root>
+        <Toast.Viewport className={styles.Viewport} />
+      </Toast.Provider>
+    </>
   );
 };
 
