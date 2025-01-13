7b959751bb1a31294a3b81b228e1d30ec7077515
Subash Rodrigo <subash.rodrigo@gmail.com>
2025-01-14 01:51:48 +0530

    Add sorting functionality to IssuesPage with dynamic column headers.

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,15 +1,27 @@
 import { IssueStatusBadge } from "@/app/components";
 import prisma from "@/prisma/client";
-import { Status } from "@prisma/client";
+import { Issue, Status } from "@prisma/client";
 import { Flex, Heading, Table } from "@radix-ui/themes";
+import NextLink from "next/link";
 import { GiBugNet } from "react-icons/gi";
+import { HiSortAscending } from "react-icons/hi";
 import IssueActions from "./IssueActions";
 
 const IssuesPage = async ({
   searchParams,
 }: {
-  searchParams: { status: Status };
+  searchParams: { status: Status; orderBy: keyof Issue };
 }) => {
+  const columns: {
+    label: string;
+    value: keyof Issue;
+    className?: string;
+  }[] = [
+    { label: "Issue", value: "title" },
+    { label: "Status", value: "status", className: "hidden md:table-cell" },
+    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
+  ];
+
   const status = Object.values(Status).includes(searchParams.status)
     ? searchParams.status
     : undefined;
@@ -17,9 +29,6 @@ const IssuesPage = async ({
   const issues = await prisma.issue.findMany({
     where: {
       status,
-    },
-    orderBy: {
-      createdAt: "desc",
     },
   });
 
@@ -29,13 +38,30 @@ const IssuesPage = async ({
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Status
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Created
-            </Table.ColumnHeaderCell>
+            {columns.map((column) => (
+              <Table.ColumnHeaderCell
+                key={column.value}
+                className={column.className}
+              >
+                <NextLink
+                  href={{
+                    query: {
+                      ...searchParams,
+                      orderBy: column.value,
+                    },
+                  }}
+                >
+                  {column.label}
+                  {column.value === searchParams.orderBy && (
+                    <HiSortAscending
+                      fontSize="20"
+                      className="inline ml-1"
+                      color="teal"
+                    />
+                  )}
+                </NextLink>
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
 
