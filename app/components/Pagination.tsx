06f94ef3b11a29c35adb0ddaba340b17c4762a56
Subash Rodrigo <subash.rodrigo@gmail.com>
2025-01-15 23:55:46 +0530

    Implement client-side pagination with dynamic page navigation.

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { Button, Flex, Text } from "@radix-ui/themes";
+import { useRouter, useSearchParams } from "next/navigation";
 import {
   MdOutlineKeyboardArrowLeft,
   MdOutlineKeyboardArrowRight,
@@ -13,27 +16,51 @@ interface Props {
 }
 
 const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
   const pagesCount = Math.ceil(itemCount / pageSize);
 
   if (pagesCount <= 1) {
     return null;
   }
 
+  const changePage = (newPage: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", newPage.toString());
+    router.push(`?${params.toString()}`);
+  };
+
   return (
     <Flex align="center" gap="2">
       <Text size="2">
         Page {currentPage} of {pagesCount}
       </Text>
-      <Button variant="soft" disabled={currentPage === 1}>
+      <Button
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(1)}
+      >
         <MdOutlineKeyboardDoubleArrowLeft fontSize="18" />
       </Button>
-      <Button variant="soft" disabled={currentPage === 1}>
+      <Button
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}
+      >
         <MdOutlineKeyboardArrowLeft fontSize="18" />
       </Button>
-      <Button variant="soft" disabled={currentPage === pagesCount}>
+      <Button
+        variant="soft"
+        disabled={currentPage === pagesCount}
+        onClick={() => changePage(currentPage + 1)}
+      >
         <MdOutlineKeyboardArrowRight fontSize="18" />
       </Button>
-      <Button variant="soft" disabled={currentPage === pagesCount}>
+      <Button
+        variant="soft"
+        disabled={currentPage === pagesCount}
+        onClick={() => changePage(pagesCount)}
+      >
         <MdOutlineKeyboardDoubleArrowRight fontSize="18" />
       </Button>
     </Flex>
