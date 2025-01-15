f14fa02783a4cf948a0f1469cece0b13e981c582
Subash Rodrigo <subash.rodrigo@gmail.com>
2025-01-16 00:18:59 +0530

    Refactor Home component by removing unused Pagination import and simplifying props

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
 import { Text } from "@radix-ui/themes";
-import Pagination from "./components/Pagination";
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
-  return (
-    <Text>
-      Home Dashboard
-    </Text>
-  );
+export default function Home() {
+  return <Text>Home Dashboard</Text>;
 }
