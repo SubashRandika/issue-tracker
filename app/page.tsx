dc6ba63035764041a7f0b3bc401c31392ce5030d
Subash Rodrigo <subash.rodrigo@gmail.com>
2025-01-20 02:40:46 +0530

    Refactor Home component to group issues by status and count them, enhancing data retrieval efficiency.

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,37 @@
 import prisma from "@/prisma/client";
+import { Status } from "@prisma/client";
 import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 
 const Home = async () => {
-  const open = await prisma.issue.count({
+  const issueCounts = await prisma.issue.groupBy({
+    by: ["status"],
+    _count: {
+      status: true,
+    },
     where: {
-      status: "OPEN",
+      status: {
+        in: Object.values(Status),
+      },
     },
   });
 
-  const inProgress = await prisma.issue.count({
-    where: {
-      status: "IN_PROGRESS",
-    },
-  });
-
-  const closed = await prisma.issue.count({
-    where: {
-      status: "CLOSED",
-    },
-  });
+  const issues = issueCounts.reduce(
+    (acc, issue) => ({
+      ...acc,
+      [issue.status]: issue._count.status,
+    }),
+    { OPEN: 0, IN_PROGRESS: 0, CLOSED: 0 }
+  );
 
   return (
     <>
       <LatestIssues />
-      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+      <IssueSummary
+        open={issues[Status.OPEN]}
+        inProgress={issues[Status.IN_PROGRESS]}
+        closed={issues[Status.CLOSED]}
+      />
     </>
   );
 };
