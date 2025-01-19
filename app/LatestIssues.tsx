3b74c07219bd5c5b90a262c50325d6999e789efc
Subash Rodrigo <subash.rodrigo@gmail.com>
2025-01-19 13:30:23 +0530

    Refactor LatestIssues component to replace Table with Flex layout for improved readability and styling.

--- app/LatestIssues.tsx
+++ app/LatestIssues.tsx
@@ -1,11 +1,13 @@
 import prisma from "@/prisma/client";
 import {
   Avatar,
+  Box,
   Card,
   Flex,
   Heading,
   Link,
-  Table
+  Separator,
+  Text,
 } from "@radix-ui/themes";
 import { IssueStatusBadge } from "./components";
 
@@ -20,37 +22,33 @@ const LatestIssues = async () => {
 
   return (
     <Card>
-      <Table.Root>
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell>
-              <Heading>Latest Issues</Heading>
-            </Table.ColumnHeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {latesIssues.map((issue) => (
-            <Table.Row key={issue.id}>
-              <Table.Cell p="2">
-                <Flex align="center" gap="3">
-                  <Avatar
-                    size="3"
-                    src={issue.assignedToUser?.image!}
-                    fallback="?"
-                    radius="full"
-                  />
-                  <Flex direction="column" gap="2" align="start">
-                    <Link href={`/issues/${issue.id}`} color="blue">
-                      {issue.title}
-                    </Link>
-                    <IssueStatusBadge status={issue.status} />
-                  </Flex>
-                </Flex>
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
+      <Heading mx="3" mt="1" mb="3">
+        Latest Issues
+      </Heading>
+      <Box px="3">
+        <Separator orientation="horizontal" size="4" />
+        {latesIssues.map((issue, index) => (
+          <Flex direction="column" key={issue.id} gap="3" mt="2">
+            <Flex align="center" gap="2">
+              <Avatar
+                size="3"
+                src={issue.assignedToUser?.image!}
+                fallback="?"
+                radius="full"
+              />
+              <Flex direction="column" gap="1" align="start">
+                <Link href={`/issues/${issue.id}`} color="blue">
+                  <Text size="2">{issue.title}</Text>
+                </Link>
+                <IssueStatusBadge status={issue.status} />
+              </Flex>
+            </Flex>
+            {index !== latesIssues.length - 1 && (
+              <Separator orientation="horizontal" size="4" />
+            )}
+          </Flex>
+        ))}
+      </Box>
     </Card>
   );
 };
